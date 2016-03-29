e229a8c28fd17e1ae5d8639b2c838591c88ff61d
HOkn <haruokny@gmail.com>
2016-03-30 04:03:04 +0900

    added articles and comments from railsguide tutorial

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+def create
+  @article = Article.find(params[:article_id])
+  @comment = @article.comments.create(comment_params)
+  if @comment.save
+    redirect_to article_path(@article)
+  end
+
+end
+
+end
+
+private
+def comment_params
+  params.require(:comment).permit(:commenter, :body)
+  
+end
